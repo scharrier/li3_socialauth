b9a220eb2da0e00d5515bed9d6797b3526b39436
scharrier <scharrier@gmail.com>
2013-02-27 23:37:23 +0100

    Facebook adapter.

---
+++ extensions/adapter/security/auth/Facebook.php
@@ -0,0 +1,30 @@
+<?php
+namespace li3_socialauth\extensions\adapter\security\auth ;
+
+use li3_socialauth\extensions\adapter\security\auth\OAuth2 ;
+
+/**
+ * Facebook authentication.
+ */
+class Facebook extends OAuth2 {
+
+	/**
+	 * Service name
+	 */
+	const NAME = 'facebook' ;
+
+	/**
+	 * User infos URL
+	 */
+	const USER_INFO = 'https://graph.facebook.com/me' ;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param array $config [description]
+	 */
+	public function __construct(array $config = array()) {
+		$config += array('tokenKey' => 'oauth_token', 'scope' => array('email')) ;
+		parent::__construct($config) ;
+	}
+}
