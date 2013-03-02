2a7d996658e3b5f98ea6aac5e3f9055f2f108ad5
scharrier <scharrier@gmail.com>
2013-03-02 22:25:19 +0100

    Added Foursquare and Github adapters.

---
+++ extensions/adapter/security/auth/Foursquare.php
@@ -0,0 +1,30 @@
+<?php
+namespace li3_socialauth\extensions\adapter\security\auth ;
+
+use li3_socialauth\extensions\adapter\security\auth\OAuth2 ;
+
+/**
+ * Foursquare authentication.
+ */
+class Foursquare extends OAuth2 {
+
+	/**
+	 * Service name
+	 */
+	const NAME = 'foursquare' ;
+
+	/**
+	 * User infos URL
+	 */
+	const USER_INFO = 'https://api.foursquare.com/v2/users/self?v=20130302' ;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param array $config [description]
+	 */
+	public function __construct(array $config = array()) {
+		$config += array('tokenKey' => 'oauth_token', 'scope' => array()) ;
+		parent::__construct($config) ;
+	}
+}
---
+++ extensions/adapter/security/auth/Github.php
@@ -0,0 +1,30 @@
+<?php
+namespace li3_socialauth\extensions\adapter\security\auth ;
+
+use li3_socialauth\extensions\adapter\security\auth\OAuth2 ;
+
+/**
+ * Github authentication.
+ */
+class Github extends OAuth2 {
+
+	/**
+	 * Service name
+	 */
+	const NAME = 'github' ;
+
+	/**
+	 * User infos URL
+	 */
+	const USER_INFO = 'https://api.github.com/user' ;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param array $config [description]
+	 */
+	public function __construct(array $config = array()) {
+		$config += array('tokenKey' => 'oauth_token', 'scope' => array('user')) ;
+		parent::__construct($config) ;
+	}
+}
